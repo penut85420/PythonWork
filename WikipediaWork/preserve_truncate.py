ecec5d44f907e0f53f614223665d3f1aa1267841
Penut Chen <penut85420@gmail.com>
2018-05-31 03:10:59 +0800

    Add category truncate

--- WikipediaWork/preserve_truncate.py
+++ WikipediaWork/preserve_truncate.py
@@ -13,8 +13,8 @@ preBegin = '<text xml:space="preserve">'
 preEnd = "=="
 baseFileSize = 4000000
 fileNameFormat = "%s%s%.4d.xml"
+category = re.compile("\[\[Category:(.*?)[\|\]]")
 
-os.makedirs(outputDir)
 for dirPath, dirName, fileNames in os.walk(inputDir):
     fid = 1
     fout = open(fileNameFormat % (outputDir, fileHead, fid), "w", encoding="UTF-8")
@@ -22,10 +22,22 @@ for dirPath, dirName, fileNames in os.walk(inputDir):
     for fileName in fileNames:
         with open(dirPath + "\\" + fileName, "r", encoding="UTF-8") as fin:
             record = False
+            category_list = []
             for line in fin:
                 if line.find(pageEnd) != -1:
                     fout.write("</preserve>\n")
                     record = False
+                elif line.find(preBegin) != -1:
+                    fout.write(line.replace(preBegin, "<preserve>\n").strip() + "\n")
+                    record = True
+                elif line.find(preEnd) != -1:
+                    record = False
+                elif line.find(titleBegin) != -1:
+                    fout.write("<category>\n")
+                    for c in category_list:
+                        fout.write(c + " ")
+                    fout.write("\n</category>\n")
+                    category_list = []
                     if fout.tell() > baseFileSize:
                         fout.write("</pageset>\n")
                         print(fileNameFormat % (outputDir, fileHead, fid))
@@ -33,13 +45,10 @@ for dirPath, dirName, fileNames in os.walk(inputDir):
                         fout.close()
                         fout = open(fileNameFormat % (outputDir, fileHead, fid), "w", encoding="UTF-8")
                         fout.write(xmlHeader)
-                elif line.find(preBegin) != -1:
-                    fout.write(line.replace(preBegin, "<preserve>\n").strip() + "\n")
-                    record = True
-                elif line.find(preEnd) != -1:
-                    record = False
-                elif line.find(titleBegin) != -1:
                     fout.write(line.strip() + "\n")
                 elif record: fout.write(line)
+                elif category.search(line) is not None:
+                    category_list.append(category.search(line).group(1))
+
 
 print("Preserve split done.")
