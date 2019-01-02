26f2b3de79744e27a16955f15a5e59e5fd911a08
PenutChen <Penut@PenutDesktop>
2019-01-03 00:42:00 +0800

    Update

--- FileScript/checkgit.py
+++ FileScript/checkgit.py
@@ -6,5 +6,5 @@ for dirPath, dirList, fileList in os.walk('./'):
 	if os.path.exists(join(dirPath, '.git')):
 		print(join(dirPath, '.git'))
 		p = join(dirPath, '.git')
-		# subprocess.call(['git', '--git-dir=%s' % p, '--work-tree=%s' % dirPath, 'branch', '--set-upstream-to=origin/master', 'master'])
+		subprocess.call(['git', '--git-dir=%s' % p, '--work-tree=%s' % dirPath, 'branch', '--set-upstream-to=origin/master', 'master'])
 		subprocess.call(['git', '--git-dir=%s' % p, '--work-tree=%s' % dirPath, 'pull'])
